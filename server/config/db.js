7c05683e231dfa1a3e936d470a8763d19fc800a7
hejazinasr <123108083+hejazinasr@users.noreply.github.com>
2023-02-19 13:14:44 -0500

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = 
+{
+    //"URI":"mongodb://127.0.0.1/contacts"
+    "URI":"mongodb+srv://Arjen:<6D9lukYFyINH868e>@mongodbserver.vuncwmt.mongodb.net/contact?retryWrites=true&w=majority"
+}
